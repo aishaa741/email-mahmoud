36d707f50e003d817c52bcfe9cf77532a269d203
aishaa741 <mahmoud.aisha@iisgalvanimi.edu.it>
2025-04-11 09:00:53 +0000

    punto 3,5

--- email-mahmoud/src/app/app.component.ts
+++ email-mahmoud/src/app/app.component.ts
@@ -8,5 +8,10 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'email-mahmoud';
+  emailvett: Email[] = [];
+  Invia(email: HTMLInputElement, oggetto: HTMLInputElement, testo: HTMLInputElement): boolean {
+    console.log(email.value, oggetto.value, testo.value)
+    this.emailvett.push(new Email (email.value, oggetto.value, testo.value));
+  return false;
+  }
 }
